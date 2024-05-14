2af6707ff93e2796994c49791703d112e7ad2d8e
lakwetu-tz <enocrunk@gmail.com>
2024-05-14 06:53:56 +0300

    public change

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -11,31 +11,30 @@ const Welcome = () => {
         <div className="blur-[106px] h-56 bg-gradient-to-br from-teal-500 to-purple-400 dark:from-blue-700"></div>
         <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
       </div>
-      <div className="absolute right-4 top-4 bg-red-900 rounded-xl flex justify-center items-center">
+      <div className="absolute right-4 top-4 bg-green-900 rounded-xl flex justify-center items-center">
         <div className="relative inline-flex group">
           <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
           <Link
             to="/login"
             title="Login"
-                      className="relative inline-flex items-center justify-center px-4 py-2 text-md font-bold text-white transition-all duration-200 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+            className="relative inline-flex items-center justify-center px-4 py-2 text-md font-bold text-white transition-all duration-200 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
             role="button"
           >
             Log In
           </Link>
         </div>
-        </div>
+      </div>
 
       <div className="lg:w-[600px] mx-auto justify-center items-center text-center">
         <div className="mb-8">
-          <h1 className="text-2xl font-extrabold mb-4">Let's jump back in!</h1>
+          <h1 className="text-2xl font-extrabold mb-4">NK CNG AUTOMOTIVE LOAN!</h1>
           <p className="text-sm font-bold text-gray-400 text-wrap">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, rem recusandae soluta voluptas
-            distinctio dignissimos, fugit tempore consequuntur quae tenetur temporibus quasi voluptatibus! Eaque,
-            placeat. Possimus corporis fuga harum dignissimos?
+            Loans provided by NK CNG Automotive Limited to help you to do conversion
+            of you car from fuel to gas
           </p>
         </div>
 
-        <Link to="/details" className="bg-red-800 text-white rounded-md shadow-lg px-4 py-2">
+        <Link to="/details" className="bg-green-800 text-white rounded-md shadow-lg px-4 py-2">
           Welcome
         </Link>
       </div>
