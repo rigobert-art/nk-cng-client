118d5813109df26586e49fa656d7b8f0ac8a33a1
rigobert-art <rigobertkiata7@gmail.com>
2024-06-24 09:39:52 +0300

    Initial Commit

--- src/api/Auth.tsx
+++ src/api/Auth.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import axios from 'axios';
+import {useState} from "react";
+import axios from "axios";
 
 const authApi = () => {
-    const [ loading, setLoading ] = useState(false);
-    const [error, setError ] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
 
-    const api = axios.create({
-        baseURL: 'http://you-api-url.com/api'
-    })
-}
+	const api = axios.create({
+		baseURL: "http://127.0.0.1:4000/api/v1",
+	});
+};
